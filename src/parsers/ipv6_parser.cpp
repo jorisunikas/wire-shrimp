f91a10d05c2a5f39b2d849975d6b7386926585fc
Joris Unikas <joris.unikas21@gmail.com>
2026-05-19 15:43:21 +0300

    add ipv6 transport header skipping to get to udp and tcp

--- src/parsers/ipv6_parser.cpp
+++ src/parsers/ipv6_parser.cpp
@@ -1,5 +1,6 @@
 #include "ipv6_parser.hpp"
 
+#include <cstdint>
 #include <sstream>
 
 static std::string readIPv6Address(const uint8_t *data) {
@@ -21,6 +22,31 @@ IPv6Header IPv6Parser::parse(RawPacket &rp) {
     header.hopLimit = data[7];
     header.srcIp = readIPv6Address(data + 8);
     header.dstIp = readIPv6Address(data + 24);
+
+    // IPv6 might contain extension headers, need to skip them to access UDP/TCP
+    int offset = 40;
+    uint8_t nextHeader = header.protocol;
+
+    while (nextHeader == 0 || nextHeader == 43 || nextHeader == 44 ||
+           nextHeader == 60) {
+
+        if (ETHERNET_HEADER_SIZE + offset + 2 > rp.len)
+            break;
+
+        const uint8_t *ext = rp.data + ETHERNET_HEADER_SIZE + offset;
+
+        if (nextHeader == 44) {
+            // Fragment header is always 8 bytes, no length field
+            nextHeader = ext[0];
+            offset += 8;
+        } else {
+            nextHeader = ext[0];
+            // first byte contains the length
+            offset += (ext[1] + 1) * 8;
+        }
+    }
+
+    header.protocol = nextHeader;
 
     return header;
 }
